faab43eeb457d6249f44f5e1dd35f587e0975636
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-04-30 16:28:25 +0500

    update

--- final_win.py
+++ final_win.py
@@ -9,10 +9,11 @@ from instr import *
 
 
 class FinalWin(QWidget):
-    def __init__(self):
+    def __init__(self, exp):
         ''' окно, в котором проводится опрос '''
         super().__init__()
 
+        self.exp = exp
 
         # создаём и настраиваем графические элементы:
         self.initUI()
@@ -41,4 +42,13 @@ class FinalWin(QWidget):
     def set_appear(self):
         self.setWindowTitle(txt_finalwin)
         self.resize(win_width, win_height)
-        self.move(win_x, win_y)
+        self.move(win_x, win_y) 
+
+    def result(self):
+        if self.exp.age < 7:
+            self.index = 0
+            return "нет данных такого возраста"
+        self.index = (4 * (int(self.exp.test1) + int(self.exp.test2) + int(self.exp.test3)) - 200)/10
+        if self.exp.age  == 7 or self.exp.age ==8: 
+            if self.index >= 21:
+                return txt_res1
