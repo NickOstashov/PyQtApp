09d15050d4a0dade509b2a0dfcaf6ddf9c2f3397
NickOstashov <126785534+OstNi@users.noreply.github.com>
2023-04-30 16:27:49 +0500

    + com

--- second_win.py
+++ second_win.py
@@ -10,6 +10,13 @@ from PyQt5.QtWidgets import (
 from instr import *
 from final_win import *
 
+class Experiment:
+    def __init__(self, age, test1, test2, test3):
+        self.age = age
+        self.test1 = test1
+        self.test2 = test2
+        self.test3 = test3
+
 
 class TestWin(QWidget):
     def __init__(self):
@@ -98,7 +105,11 @@ class TestWin(QWidget):
     
     def next_click(self):
         self.hide()
-        self.fw = FinalWin()
+        self.exp = Experiment(int(self.line_age.text()), 
+                              self.line_test1.text(), 
+                              self.line_test2.text(), 
+                              self.line_test3.text())
+        self.fw = FinalWin(self.exp)
 
 
     def connects(self):
